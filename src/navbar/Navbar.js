023fd74a3523d2f01bc99790c2d391841b675486
yorps <7090963+yorps@users.noreply.github.com>
2020-02-21 18:23:28 +0100

    removing json formatter from navbar

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -69,7 +69,6 @@ export class Navbar extends React.Component {
                         <NavItem path="/" name="Home" />
                         <NavItem path="/datetool" name="Dates" />
                         <NavItem path="/base64decoder" name="Base64-Decoder" />
-                        <NavItem path="/json" name="JSON" />
                         <NavItem path="/converter" name="Converter" />
                         <NavItem path="/generator" name="Hash Generator" />
 
