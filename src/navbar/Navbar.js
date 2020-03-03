bf499681f3ffe6f40cdb979ca24aa40a4317e632
yorps <7090963+yorps@users.noreply.github.com>
2020-03-03 18:59:10 +0100

    include jquery and bootstrap.js for NavBar

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import 'jquery/dist/jquery.slim';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -60,7 +63,7 @@ export class Navbar extends React.Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">{appName}</a>
-                <button className="navbar-toggler" type="button" data-toggle="navbar-collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"/>
                 </button>
 
